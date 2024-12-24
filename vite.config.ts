50fabc24fc2e7276be9b3ec853083da075e70b08
asierwave <125518587+asierwave@users.noreply.github.com>
2024-12-24 02:46:30 +0100

    k

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/BCNecommerce/', // Correcto para GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
     proxy: {
-      // Mantén el proxy si usas un servidor API local, de lo contrario elimínalo
-      '/api': 'http://localhost:5000', 
+      '/api': 'http://localhost:3000',
     },
   },
 });

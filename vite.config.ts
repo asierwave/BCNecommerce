6c2fedc59d1d2706d178c87555e6589ff14c2642
asierwave <125518587+asierwave@users.noreply.github.com>
2024-12-25 02:24:33 +0100

    s

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Usa rutas relativas para que funcionen en cualquier entorno
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
+

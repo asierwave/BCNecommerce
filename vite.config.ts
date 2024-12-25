a11807f7e92339f7eb47e35edb8f6b9b3cde4f7f
asierwave <125518587+asierwave@users.noreply.github.com>
2024-12-25 04:04:53 +0100

    Actualizar vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/BCNecommerce/', // Reemplaza 'BCNecommerce' con el nombre de tu repositorio
+  base: 'https://asierwave.github.io/BCNecommerce/', // Reemplaza 'BCNecommerce' con el nombre de tu repositorio
   optimizeDeps: {
     exclude: ['lucide-react'],
   },

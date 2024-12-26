b24eed48bb5eaa373678c54525756e52fc6d2995
asierwave <125518587+asierwave@users.noreply.github.com>
2024-12-27 00:41:15 +0100

    Update create-checkout-session.js

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,25 +1,16 @@
-// Backend: /netlify/functions/create-checkout-session.js
-require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event, context) => {
   try {
     const { items } = JSON.parse(event.body);
 
-    if (!items || !Array.isArray(items)) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: "Invalid items array" }),
-      };
-    }
-
     const lineItems = items.map(item => ({
       price_data: {
-        currency: 'usd', // Puedes hacer esto dinámico si necesitas manejar varias monedas
+        currency: 'usd',
         product_data: {
           name: item.name,
         },
-        unit_amount: item.price * 100, // Stripe espera valores en centavos
+        unit_amount: item.price * 100,
       },
       quantity: item.quantity,
     }));
@@ -37,7 +28,6 @@ exports.handler = async (event, context) => {
       body: JSON.stringify({ id: session.id }),
     };
   } catch (error) {
-    console.error(error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message }),

428a85e64d1052127c300ea131a06caf83250585
asierwave <125518587+asierwave@users.noreply.github.com>
2024-12-26 23:49:30 +0100

    subir

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_PUBLIC_KEY);
 
 exports.handler = async (event, context) => {
   try {

e20a03a7aef1105bd9f21d98cf16c492645a4ec6
asierwave <125518587+asierwave@users.noreply.github.com>
2024-12-26 23:52:59 +0100

    Update create-checkout-session.js

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_PUBLIC_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event, context) => {
   try {
@@ -26,6 +26,7 @@ exports.handler = async (event, context) => {
     return {
       statusCode: 200,
       body: JSON.stringify({ id: session.id }),
+      console.log(process.env.STRIPE_SECRET_KEY),
     };
   } catch (error) {
     return {
